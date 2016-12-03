e4a91b27e69432ed50235784da7e0dfd576ccc2e
ishant3 <upadhyay.ishant@yahoo.com>
2016-12-03 19:54:51 +0100

    working Examples
    wcf and android example works.

---
+++ AndroidApplication/AndroidApplication/AndroidApplicationService.svc.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace AndroidApplication
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AndroidApplicationSevice" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select AndroidApplicationSevice.svc or AndroidApplicationSevice.svc.cs at the Solution Explorer and start debugging.
+    public class AndroidApplicationSevice : IAndroidApplicationService
+    {
+        //    Account myAccount;
+        //    RestaurantOrderService()
+        //    {
+
+        //        myAccount = new Account()
+        //        {
+        //            Email = "test@test.com",
+        //            Password = "password123."
+        //        };
+        //    }
+        //    public bool Authenticate(string email, string password)
+        //    {
+        //        if (myAccount.Email == email && myAccount.Password == password)
+        //        {
+        //            return true;
+        //        }
+        //        return false;
+        //    }
+
+        //    public Account GetAccount(string email, string password)
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+        //}
+        public bool Authenticate(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account GetAccount(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        //for test
+
+        public HelloWorldData GetHelloData(HelloWorldData helloWorldData)
+        {
+            if (helloWorldData == null)
+            {
+                throw new ArgumentNullException("helloWorldData");
+            }
+
+            if (helloWorldData.SayHello)
+            {
+                helloWorldData.Name = String.Format("Hello World to {0}.", helloWorldData.Name);
+            }
+            return helloWorldData;
+        }
+
+        public string SayHelloTo(string name)
+        {
+            return string.Format("Hello World to you, {0}", name);
+        }
+    }
+}
---
+++ AndroidApplication/AndroidApplication/IAndroidApplicationService.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace AndroidApplication
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IAndroidApplicationService
+    {
+        // TODO: Add your service operations here
+        [OperationContract]
+        bool Authenticate(string email, string password);
+        [OperationContract]
+        Account GetAccount(string email, string password);
+
+        //for test
+        [OperationContract]
+        string SayHelloTo(string name);
+
+        [OperationContract]
+        HelloWorldData GetHelloData(HelloWorldData helloWorldData);
+
+    }
+
+    //for test
+    [DataContract]
+    public class HelloWorldData
+    {
+        public HelloWorldData()
+        {
+            Name = "Hello ";
+            SayHello = false;
+        }
+
+        [DataMember]
+        public bool SayHello { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+    }
+
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "RestaurantOrder.ContractType".
+    [DataContract]
+    public class MenuProduct
+    {
+        [DataMember]
+        public int Productid { get; set; }
+        [DataMember]
+        public string ProductName { get; set; }
+        [DataMember]
+        public string Productdescription { get; set; }
+        [DataMember]
+        public decimal ProductPrice { get; set; }
+        [DataMember]
+        public int Quantity { get; set; }
+    }
+    [DataContract]
+    public class Account
+    {
+        [DataMember]
+        public int Accountid { get; set; }
+        [DataMember]
+      public string Username { get; set; }
+        [DataMember]
+        public string Password { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+     
+
+
+    }
+    [DataContract]
+    public class Restaurant
+    {
+        [DataMember]
+        public int Restaurantid { get; set; }
+        [DataMember]
+        public string Restaurantname { get; set; }
+        [DataMember]
+        public string Location { get; set; }
+        [DataMember]
+        public int nbrOfTables { get; set; }
+        
+    }
+    [DataContract]
+    public class Order
+    {
+        [DataMember]
+        public int Orderid { get; set; }
+        [DataMember]
+        public string Comment { get; set; }
+        [DataMember]
+        public int Accountid { get; set; }
+        [DataMember]
+        public int Restaurantid { get; set; }
+        [DataMember]
+        public int TableNbr { get; set; }
+        [DataMember]
+        public DateTime OrderDate { get; set; }
+        [DataMember]
+        public List<MenuProduct> orderList { get; set; }
+
+    }
+}
